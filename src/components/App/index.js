08228297e4f09d14901dfe8bcc7fc987f7517110
Joshua Farayola <farayolajoshua@gmail.com>
2020-06-01 03:19:58 +0100

    Moving forward

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+import Navigation from '../Navigation';
+import HomePage from '../Home';
+import LandingPage from '../Landing';
+import AdminPage from '../Admin';
+import AccountPage from '../Account';
+import PasswordForgetPage from '../PasswordForget';
+import SignInPage from '../SignIn';
+import SignUpPage from '../SignUp';
  
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+  <Router>
+    <Navigation />
+
+    <hr />
+
+    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+  </Router>
 );
  
 export default App;
